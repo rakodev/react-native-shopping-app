105193a58d23b7304346667b61c18546c0888a06
Ramazan KORKMAZ <rakodev@gmail.com>
2019-11-06 12:13:59 +0100

    Add navigation listener to reload products every time we come back to the products screens

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -24,6 +24,7 @@ const ProductsOverviewScreen = props => {
   const dispatch = useDispatch();
 
   const loadProducts = useCallback(async () => {
+    console.log("LOAD PRODUCTS");
     setError(null);
     setIsLoading(true);
     try {
@@ -33,6 +34,16 @@ const ProductsOverviewScreen = props => {
     }
     setIsLoading(false);
   }, [dispatch, setIsLoading, setError]);
+
+  useEffect(() => {
+    const willFocusSub = props.navigation.addListener(
+      "willFocus",
+      loadProducts
+    );
+    return () => {
+      willFocusSub.remove();
+    };
+  }, [loadProducts]);
 
   useEffect(() => {
     loadProducts();
