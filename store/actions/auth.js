eefd5f6c10c28ae2447362055391e681ff296548
Ramazan KORKMAZ <rakodev@gmail.com>
2019-11-06 15:43:17 +0100

    WIP: Sign-in users

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,9 +1,12 @@
 export const SIGNUP = "SIGNUP";
+export const LOGIN = "LOGIN";
+const API_KEY = "AIzaSyBk39nqHSixrxip0U2mBlz6LZymM3JRoqo";
 
 export const signup = (email, password) => {
   return async dispatch => {
     const response = await fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBk39nqHSixrxip0U2mBlz6LZymM3JRoqo",
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" +
+        API_KEY,
       {
         method: "POST",
         headers: {
@@ -24,5 +27,33 @@ export const signup = (email, password) => {
     const resData = await response.json();
     console.log(resData);
     dispatch({ type: SIGNUP });
+  };
+};
+
+export const login = (email, password) => {
+  return async dispatch => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
+        API_KEY,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({ type: LOGIN });
   };
 };
