91132c4d8091e1387315843e6799ac027315a191
Ramazan KORKMAZ <rakodev@gmail.com>
2019-11-06 11:37:27 +0100

    Handle fetch data error & Loading screen added

--- store/actions/products.js
+++ store/actions/products.js
@@ -9,27 +9,34 @@ export const SET_PRODUCTS = "SET_PRODUCTS";
 export const fetchProducts = () => {
   return async dispatch => {
     // any async code you want!
-    const response = await fetch(PRODUCTS_URL);
+    try {
+      const response = await fetch(PRODUCTS_URL);
 
-    const resData = await response.json();
-    const loadedProducts = [];
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
-    console.log(resData);
+      const resData = await response.json();
+      const loadedProducts = [];
 
-    for (const key in resData) {
-      loadedProducts.push(
-        new Product(
-          key,
-          "u1",
-          resData[key].title,
-          resData[key].imageUrl,
-          resData[key].description,
-          resData[key].price
-        )
-      );
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            "u1",
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      // send to custom analytics server
+      throw err;
     }
-
-    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
   };
 };
 
